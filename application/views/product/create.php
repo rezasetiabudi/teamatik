76a3919b3714fca1d53fe1ffd69cc38bd032a66f
dinaputrikh <dinaputrikh@gmail.com>
2019-09-26 14:23:30 +0700

    product dina

--- application/views/product/create.php
+++ application/views/product/create.php
@@ -1,31 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add Product</title>
+<meta charset="UTF-8">
+    <title>Inventory Management System | Teamatik</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/png" href="#">
+    <link rel="stylesheet" href="<?php echo base_url('assets') ?>/style.css" type="text/css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1 class="enter">ADD PRODUCT</h1>
+<!-- header & setting account -->
+<div class="grid">
+    <header class="header">
+        <i class="fas fa-bars header__menu"></i>
+        <div class="header__search">
+            <input class="header__input" placeholder="Search..." />
+        </div>
+        <div class="header__avatar">
+            <i class="fas fa-user-cog fa-2x"></i>
+            <div class="dropdown">
+                <ul class="dropdown__list">
+                    <li class="dropdown__list-item">
+                        <span class="dropdown__icon"><i class="far fa-user"></i></span>
+                        <span class="dropdown__title">my profile</span>
+                    </li>
+                    <li class="dropdown__list-item">
+                        <span class="dropdown__icon"><i class="fas fa-clipboard-list"></i></span>
+                        <span class="dropdown__title">my account</span>
+                    </li>
+                    <li class="dropdown__list-item">
+                        <span class="dropdown__icon"><i class="fas fa-sign-out-alt"></i></span>
+                        <a class="dropdown__title" href="<?= base_url('/Home/logout') ?>">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 
-    <div class="form-group">
-        <form method="post">
-            Product Name: <input type="text" name="name"><br>
-            <select class = "form-control" name="category_id">
-            <?php
-                $count = count($kategori);
-                for($i = 0; $i<$count; $i++){
-                    echo '<option value="'.$kategori[$i][id].'">'.$kategori[$i][name].'</option>';
-                    
-                }
-            ?>
-            </select>
-            Purchase Year: <input type="date" name="purchase"><br>
-            Price : <input type="text" name="price"><br>
-            <input type="submit" name="save" value="save">
-        </form>
+    <?php $this->load->view("template/sidenavbar.php") ?>
+
+    <main class="main">
+        <div class="container">
+            <a href="<?php echo base_url() ?>/Product/index" class="backButton btn btn-danger">BACK</a>
+            <div class="form-group">
+                <form method="post">
+                <input  class="form-control input100" type="text" name="name" placeholder="Product Name"><br>
+                <select class = "form-control" name="category_id">
+                    <?php
+                        $count = count($kategori);
+                        for($i = 0; $i<$count; $i++){
+                            echo '<option value="'.$kategori[$i][id].'">'.$kategori[$i][name].'</option>';
+                        }
+                    ?>
+                </select>
+                <input class="form-control input100" type="date" name="purchase"><br>
+                <input class="form-control input100" type="text" name="price" placeholder="Product Price"><br>
+                <input type="submit" name="save" value="save" class="form-control btn btn-info">
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+            <p><span class="footer__copyright">&copy;</span> 2019 Teamatik</p>
+        </footer>
     </div>
-    <a href = "<?php echo base_url()?>/Product/index">BACK</a>
+    <!-- partial -->
+    <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+    <script src='https://www.amcharts.com/lib/3/amcharts.js'></script>
+    <script src='https://www.amcharts.com/lib/3/serial.js'></script>
+    <script src='https://www.amcharts.com/lib/3/themes/light.js'></script>
+    <script src="<?php echo base_url() ?>/assets/script.js"></script>
 </body>
 </html>
