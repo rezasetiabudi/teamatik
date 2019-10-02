895c8b65e29f7e84ee96e28439ed1d12b3dcdbd8
dinaputrikh <dinaputrikh@gmail.com>
2019-10-03 06:46:02 +0700

    update

--- application/views/product/create.php
+++ application/views/product/create.php
@@ -48,7 +48,9 @@
             <a href="<?php echo base_url() ?>/Product/index" class="backButton btn btn-danger">BACK</a>
             <div class="form-group">
                 <form method="post">
+                <label for="name">Product Name</label>
                 <input  class="form-control input100" type="text" name="name" placeholder="Product Name"><br>
+                <label for="category_id">Category</label>
                 <select class = "form-control" name="category_id">
                     <?php
                         $count = count($kategori);
@@ -57,7 +59,9 @@
                         }
                     ?>
                 </select>
+                <label for="purchase">Purchase Date</label>
                 <input class="form-control input100" type="date" name="purchase"><br>
+                <label for="price">Product Price</label>
                 <input class="form-control input100" type="text" name="price" placeholder="Product Price"><br>
                 <input type="submit" name="save" value="save" class="form-control btn btn-info">
                 </form>
