3406896756eafe7061b7490e6e3b5afd88735018
Pragoardhi <ardhiprago00@gmail.com>
2019-10-24 13:34:04 +0700

    grocery crud

--- application/views/employee/index.php
+++ application/views/employee/index.php
@@ -6,6 +6,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
+    <?php
+    foreach ($css_files as $file) : ?>
+        <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+
+    <?php endforeach; ?>
+
     <meta charset="UTF-8">
     <title>Inventory Management System | Teamatik</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
@@ -61,57 +67,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
             <div class="container">
-                <a href="<?php echo base_url() ?>" class="backButton btn btn-danger">BACK</a>
-                <a href="<?php echo base_url() ?>Employee/create" class="addButton btn btn-primary">Add</a>
-                <div class="table">
-	                <table  id="example" class="table table-striped table-bordered" style="width:100%">                        
-                        <thead>
-                            <tr>
-                                <th> # </th>
-                                <th> Name </th>
-                                <th> Email </th>
-                                <th> Phone Number </th>
-                                <th> Position </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        $count = count($list);
-                        for ($i = 0; $i < $count; $i++) {
-                            $x = $i + 1;
-                            echo '<tr>';
-                            echo '<td>' . $x . '</td>';
-                            echo '<td>' . $list[$i]["name"] . '</td>';
-                            echo '<td>' . $list[$i]["email"] . '</td>';
-                            if($list[$i]["phone"] == null){
-                                echo '<td> - </td>';
-                            }else{
-                                echo '<td>' . $list[$i]["phone"] . '</td>';
-                            }
-                            echo '<td>' . $list[$i]["position_id"] . '</td>';
-
-                            echo '</tr>';
-                        }
-                        ?>
-                        </tbody>
-                    </table>
-                </div>
+                <?php echo $output; ?>
             </div>
-
-
-
-        </main>
-
-        <footer class="footer">
-            <p><span class="footer__copyright">&copy;</span> 2019 Teamatik</p>
-        </footer>
     </div>
-    <!-- partial -->
-    <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
-    <script src='https://www.amcharts.com/lib/3/amcharts.js'></script>
-    <script src='https://www.amcharts.com/lib/3/serial.js'></script>
-    <script src='https://www.amcharts.com/lib/3/themes/light.js'></script>
-    <script src="<?php echo base_url() ?>/assets/script.js"></script>
+
+
+
+    </main>
+
+    <footer class="footer">
+        <p><span class="footer__copyright">&copy;</span> 2019 Teamatik</p>
+    </footer>
+    </div>
 </body>
+<?php foreach ($js_files as $file) : ?>
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+<script src="<?php echo base_url() ?>/assets/script.js"></script>
 
 </html>
