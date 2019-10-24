402e3fe1782c5c20d59fb67ac14541247b514e8d
kevinchristiann98 <kevinchristiann98@gmail.com>
2019-10-24 13:51:20 +0700

    update grocery crud

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -1,53 +1,66 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Employee extends CI_Controller {
-	public function index()
-	{
-            $this->load->helper('url'); 
-            $this->load->library('Grocery_CRUD');   
-            if($this->session->userdata('status') != "login"){
-			$this->load->view('login_page');
-            }
-            else {
-                  $crud = new Grocery_CRUD();  
-                  $crud->set_theme('datatables');    
+class Employee extends CI_Controller
+{
+      function __construct()
+      {
+            parent::__construct();
+
+            /* Standard Libraries of codeigniter are required */
+            $this->load->database();
+            $this->load->helper('url');
+            /* ------------------ */
+
+            $this->load->library('grocery_CRUD');
+      }
+      public function index()
+      {
+            if ($this->session->userdata('status') != "login") {
+                  $this->load->view('login_page');
+            } else {
+                  // $this->load->model('employee_model');
+                  // $employee['list'] = $this->employee_model->getEmployee();
+                  // $this->load->view('employee/index', $employee);
+                  $crud = new grocery_CRUD();
+
+                  // Seriously! This is all the code you need!
                   $crud->set_table('employee');
+                  $crud->set_subject('employee');
+                  $crud->set_relation('position_id', 'position', 'name');
                   $output = $crud->render();
-                  $this->load->view('employee/index',$output);
-                  // $this->_example_output(output);
-                  // $this->load->view('template/sidenavbar.php',$output);
+
+
+                  $this->load->view('employee/index', $output);
+                  // echo "<pre>";
+                  // print_r($output);
+                  // echo "</pre>";
+                  // die();
             }
       }
-      
-      // public function _example_output($output){
-      //       $this->load->view('template/sidenavbar.php',$output);
-      // }
 
-	public function create()
-	{
+      public function create()
+      {
 
-            if($this->session->userdata('status') != "login"){
-			$this->load->view('login_page');
-            }
-            else {
+            if ($this->session->userdata('status') != "login") {
+                  $this->load->view('login_page');
+            } else {
                   $this->load->model('position_model');
                   $position['posisi'] = $this->position_model->getList();
-                  $this->load->view('employee/create',$position);
-                  if($this->input->post('save'))
-                  {
+                  $this->load->view('employee/create', $position);
+                  if ($this->input->post('save')) {
                         //get form's data and store in local varable
-                        $name=$this->input->post('name');
-                        $email=$this->input->post('email');
-                        $phone=$this->input->post('phone');
-                        $position=$this->input->post('position');
-                        $status=$this->input->post('status');
+                        $name = $this->input->post('name');
+                        $email = $this->input->post('email');
+                        $phone = $this->input->post('phone');
+                        $position = $this->input->post('position');
+                        $status = $this->input->post('status');
 
                         $this->load->model('employee_model');
                         //call saverecords method of Hello_Model and pass variables as parameter
-                        $this->employee_model->saverecords($name,$email,$phone,$position,$status);	
+                        $this->employee_model->saverecords($name, $email, $phone, $position, $status);
                         echo "Records Saved Successfully";
                   }
             }
-	}
+      }
 }
--- application/views/employee/index.php
+++ application/views/employee/index.php
@@ -6,6 +6,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
+    <?php
+    foreach ($css_files as $file) : ?>
+        <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+
+    <?php endforeach; ?>
+
     <meta charset="UTF-8">
     <title>Inventory Management System | Teamatik</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
@@ -17,7 +23,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    
 
 </head>
 
@@ -62,44 +67,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
             <div class="container">
-                <!-- <a href="<?php echo base_url() ?>" class="backButton btn btn-danger">BACK</a> -->
-                <!-- <a href="<?php echo base_url() ?>Employee/create" class="addButton btn btn-primary">Add</a> -->
-                        <?php
-                        // $count = count($list);
-                        // for ($i = 0; $i < $count; $i++) {
-                        //     $x = $i + 1;
-                        //     echo '<tr>';
-                        //     echo '<td>' . $x . '</td>';
-                        //     echo '<td>' . $list[$i]["name"] . '</td>';
-                        //     echo '<td>' . $list[$i]["email"] . '</td>';
-                        //     if($list[$i]["phone"] == null){
-                        //         echo '<td> - </td>';
-                        //     }else{
-                        //         echo '<td>' . $list[$i]["phone"] . '</td>';
-                        //     }
-                        //     echo '<td>' . $list[$i]["position_id"] . '</td>';
-
-                        //     echo '</tr>';
-                        // }
-
-                        echo $output;
-                        ?>
+                <?php echo $output; ?>
             </div>
-
-
-
-        </main>
-
-        <footer class="footer">
-            <p><span class="footer__copyright">&copy;</span> 2019 Teamatik</p>
-        </footer>
     </div>
-    <!-- partial -->
-    <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
-    <script src='https://www.amcharts.com/lib/3/amcharts.js'></script>
-    <script src='https://www.amcharts.com/lib/3/serial.js'></script>
-    <script src='https://www.amcharts.com/lib/3/themes/light.js'></script>
-    <script src="<?php echo base_url() ?>/assets/script.js"></script>
+
+
+
+    </main>
+
+    <footer class="footer">
+        <p><span class="footer__copyright">&copy;</span> 2019 Teamatik</p>
+    </footer>
+    </div>
 </body>
+<?php foreach ($js_files as $file) : ?>
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+<script src="<?php echo base_url() ?>/assets/script.js"></script>
 
 </html>
