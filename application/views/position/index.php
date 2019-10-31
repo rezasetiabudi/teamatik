64754f48a70cd49037aed8dc002a86868f7ad75f
kevinchristiann98 <kevinchristiann98@gmail.com>
2019-10-31 15:21:18 +0700

    add delete department,position

--- application/views/position/index.php
+++ application/views/position/index.php
@@ -38,11 +38,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
         </thead>
         <tbody>
           <?php   
+          $i= 1;
             foreach($list as $rows){
           ?>
             <tr>
               <td>
-                <?php echo 1?>
+                <?php echo $i?>
               </td>
               <td>
                 <?php echo $rows['name']?>
@@ -51,10 +52,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 <?php echo $rows['department_id']?>
               </td>
               <td>
-              <a href="<?php echo base_url('Position/update/')?><?php echo $rows['id']?>">Update</a>
-              </td>
+              <a href="<?php echo base_url('Position/update/')?><?php echo $rows['id']?>"><span class="glyphicon glyphicon-cog"></span></a>
+              &nbsp
+              &nbsp
+              &nbsp
+              &nbsp
+              &nbsp
+              <a href="<?php echo base_url('Position/delete/')?><?php echo $rows['id']?>"><span class="glyphicon glyphicon-trash" style="color:red"></span></a>              </td>
             </tr>
-          <?php }?>
+          <?php $i++;}?>
         </tbody>
     </table>
     </section>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -66,7 +66,7 @@
 <script src="<?php echo base_url();?>adminlte2/dist/js/demo.js"></script>
 <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="<?php echo base_url().'Home/index/'?>" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
