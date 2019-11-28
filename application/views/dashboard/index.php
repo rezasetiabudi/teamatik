68fe12cd7b784137aaf0c140fec488b5f6862115
gemmachs <gemmacahya@gmail.com>
2019-11-28 15:23:26 +0700

    chart

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,8 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
+
 <link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?= base_url('Assets/chart.js')?>"></script>
+
 <script>
   $(document).ready(function() {
     $('#myTable').DataTable();
@@ -24,11 +27,56 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </ol>
   </section>
   <section class="content">
-    <div class ="box box-success">
-        <div class ="box-body">
-            Halo
-        </div>
+    <div class="box box-success" style="width: 100%; height: 90%">
+      <!-- <div class="box-body" style="width: 500px;height: 500px"> -->
+        <canvas id="myChart"></canvas>
+      <!-- </div> -->
     </div>
   </section>
 </div>
+
+<!-- chartjs -->
+<script>
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 23, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+</script>
+<!-- chartjs -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
 <!-- /.content -->
