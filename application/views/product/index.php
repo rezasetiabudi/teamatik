7b0ab1651fc0c732d9a6ec20a095ed4e7f02f5e9
Pragoardhi <ardhiprago00@gmail.com>
2019-11-28 14:07:39 +0700

    Update QRCode

--- application/views/product/index.php
+++ application/views/product/index.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
+
 <link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script>
@@ -81,7 +82,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
                 <td>
                   <a class="btn" href="<?php echo base_url('Product/update/') ?><?php echo $rows['id_product'] ?>"><span class="glyphicon glyphicon-cog"></span></a>
-
+                  <a class="btn" data-toggle="modal" href="" data-target="#qrModal<?php echo $rows['id_product'] ?>"><span class="glyphicon glyphicon-qrcode" style="color:black"></span></a>
                   <a class="btn" data-toggle="modal" data-target="#deleteModal<?php echo $rows['id_product'] ?>"><span class="glyphicon glyphicon-trash" style="color:red"></span></a> </td>
               </tr>
               <div class="modal fade" id="deleteModal<?php echo $rows['id_product'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -103,6 +104,42 @@ defined('BASEPATH') or exit('No direct script access allowed');
                   </div>
                 </div>
               </div>
+              <div class="modal fade" id="qrModal<?php echo $rows['id_product'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div id="printThis<?php echo $rows['id_product'] ?>" class="modal-body">
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" id="Print<?php echo $rows['id_product'] ?>" class="btn btn-primary">Print</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <script>
+                document.getElementById("Print<?php echo $rows['id_product'] ?>").onclick = function() {
+                  printElement(document.getElementById("printThis<?php echo $rows['id_product'] ?>"));
+                };
+
+                function printElement(elem) {
+                  var domClone = elem.cloneNode(true);
+
+                  var $printSection = document.getElementById("printSection<?php echo $rows['id_product'] ?>");
+
+                  if (!$printSection) {
+                    var $printSection = document.createElement("div");
+                    $printSection.id = "printSection";
+                    document.body.appendChild($printSection);
+                  }
+
+                  $printSection.innerHTML = "";
+                  $printSection.appendChild(domClone);
+                  window.print();
+                }
+                $(function() {
+                  $("#printThis<?php echo $rows['id_product'] ?>").append("<p align='center'><img class='to-print' src='http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=<?php echo $rows['id_product'] ?>&chld=H|0'/></p>");
+                })
+              </script>
+
             <?php $i++;
             } ?>
           </tbody>
@@ -111,4 +148,28 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </div>
   </section>
 </div>
+<style type="text/css">
+  @media screen {
+    #printSection {
+      display: none;
+    }
+  }
+
+  @media print {
+    body * {
+      visibility: hidden;
+    }
+
+    #printSection,
+    #printSection * {
+      visibility: visible;
+    }
+
+    #printSection {
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  }
+</style>
 <!-- /.content -->
